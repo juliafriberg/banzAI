d277901afcdf602b95635e629a9323b60e5df45d
William Dahlberg <dahlberg.william@gmail.com>
2016-04-29 14:36:49 +0200

    all test pass, no timeout

--- Graph.js
+++ Graph.js
@@ -20,50 +20,46 @@ function aStarSearch(graph, start, goal, heuristics, timeout) {
             this.f = f;
             this.g = g;
         }
+        NodeScore.prototype.toString = function () {
+            return this.node.toString();
+        };
         return NodeScore;
     }());
     var cmp = function (a, b) {
-        if (a.node === b.node) {
-            return true;
-        }
-        return false;
+        return (a.node.toString() === b.node.toString());
     };
-    var openSet = new collections.LinkedList();
     var getLowest = function () {
         var cur = openSet.firstNode;
         var ret = cur.element;
-        while (cur.next) {
+        while (cur !== null) {
             if (cur.element.f < ret.f) {
                 ret = cur.element;
             }
             cur = cur.next;
         }
-        if (cur.element.f < ret.f) {
-            ret = cur.element;
-        }
         openSet.remove(ret);
         return ret;
     };
-    openSet.add(new NodeScore(undefined, start, heuristics(start), 0));
+    var openSet = new collections.LinkedList();
+    openSet.add(new NodeScore(null, start, heuristics(start), 0));
     var closedSet = [];
     while (!openSet.isEmpty()) {
         var current = getLowest();
         if (goal(current.node)) {
             result.cost = current.g;
-            while (current.parent) {
+            while (current !== null) {
                 result.path.push(current.node);
                 current = current.parent;
             }
-            result.path.push(start);
             result.path = result.path.reverse();
-            console.log("Found path.");
+            console.log("Found path. ---------------------------------------------------");
             return result;
         }
         closedSet.push(current.node);
         var neighbours = graph.outgoingEdges(current.node);
         for (var _i = 0, neighbours_1 = neighbours; _i < neighbours_1.length; _i++) {
             var n = neighbours_1[_i];
-            if (closedSet.indexOf(n.from) > -1) {
+            if (closedSet.indexOf(n.to) > -1) {
                 continue;
             }
             var t_gScore = current.g + n.cost;
@@ -84,77 +80,3 @@ function aStarSearch(graph, start, goal, heuristics, timeout) {
     console.log("COULDN*T FIND PATH");
     return undefined;
 }
-var GridNode = (function () {
-    function GridNode(pos) {
-        this.pos = pos;
-    }
-    GridNode.prototype.add = function (delta) {
-        return new GridNode({
-            x: this.pos.x + delta.x,
-            y: this.pos.y + delta.y
-        });
-    };
-    GridNode.prototype.compareTo = function (other) {
-        return (this.pos.x - other.pos.x) || (this.pos.y - other.pos.y);
-    };
-    GridNode.prototype.toString = function () {
-        return "(" + this.pos.x + "," + this.pos.y + ")";
-    };
-    return GridNode;
-}());
-var GridGraph = (function () {
-    function GridGraph(size, obstacles) {
-        this.size = size;
-        this.walls = new collections.Set();
-        for (var _i = 0, obstacles_1 = obstacles; _i < obstacles_1.length; _i++) {
-            var pos = obstacles_1[_i];
-            this.walls.add(new GridNode(pos));
-        }
-        for (var x = -1; x <= size.x; x++) {
-            this.walls.add(new GridNode({ x: x, y: -1 }));
-            this.walls.add(new GridNode({ x: x, y: size.y }));
-        }
-        for (var y = -1; y <= size.y; y++) {
-            this.walls.add(new GridNode({ x: -1, y: y }));
-            this.walls.add(new GridNode({ x: size.x, y: y }));
-        }
-    }
-    GridGraph.prototype.outgoingEdges = function (node) {
-        var outgoing = [];
-        for (var dx = -1; dx <= 1; dx++) {
-            for (var dy = -1; dy <= 1; dy++) {
-                if (!(dx == 0 && dy == 0)) {
-                    var next = node.add({ x: dx, y: dy });
-                    if (!this.walls.contains(next)) {
-                        outgoing.push({
-                            from: node,
-                            to: next,
-                            cost: Math.sqrt(dx * dx + dy * dy)
-                        });
-                    }
-                }
-            }
-        }
-        return outgoing;
-    };
-    GridGraph.prototype.compareNodes = function (a, b) {
-        return a.compareTo(b);
-    };
-    GridGraph.prototype.toString = function () {
-        var borderRow = "+" + new Array(this.size.x + 1).join("--+");
-        var betweenRow = "+" + new Array(this.size.x + 1).join("  +");
-        var str = "\n" + borderRow + "\n";
-        for (var y = this.size.y - 1; y >= 0; y--) {
-            str += "|";
-            for (var x = 0; x < this.size.x; x++) {
-                str += this.walls.contains(new GridNode({ x: x, y: y })) ? "## " : "   ";
-            }
-            str += "|\n";
-            if (y > 0)
-                str += betweenRow + "\n";
-        }
-        str += borderRow + "\n";
-        return str;
-    };
-    return GridGraph;
-}());
--- Graph.ts
+++ Graph.ts
@@ -73,35 +73,35 @@ function aStarSearch<Node>(
       this.f = f;
       this.g = g;
     }
+
+    toString() : string {
+      return this.node.toString();
+    }
   }
 
   // Equals function to use for LinkedList, just compares the actual node in the NodeScore
   // Sent to indexOf and contains function in the LinkedList object.
   var cmp: collections.IEqualsFunction<NodeScore> = function(a, b) : boolean {
-    if (a.node === b.node){
-      return true;
-    }
-    return false;
+    return (a.node.toString() === b.node.toString())
   }
 
-  var openSet = new collections.LinkedList<NodeScore>();
   // function to get the lowest scoring node from the openSet
   var getLowest = function() : NodeScore {
     var cur = openSet.firstNode;
     var ret = cur.element;
-    while (cur.next) {
+    while (cur !== null) {
       if (cur.element.f < ret.f) {
         ret = cur.element;
       }
       cur = cur.next;
     }
-    if(cur.element.f < ret.f) {ret = cur.element;}
     openSet.remove(ret);
     return ret;
   }
 
   // add the starting node to the open set
-  openSet.add(new NodeScore(undefined, start, heuristics(start), 0));
+  var openSet = new collections.LinkedList<NodeScore>();
+  openSet.add(new NodeScore(null, start, heuristics(start), 0));
   // closed set is the ones looked at
   var closedSet: Node[] = [];
 
@@ -111,23 +111,23 @@ function aStarSearch<Node>(
     if (goal(current.node)) {
       // g-cost is already calculated on the way here
       result.cost = current.g;
-      while(current.parent) {
+      while(current !== null) {
         // the adding/reconstruction
         result.path.push(current.node);
         current = current.parent;
       }
-      result.path.push(start);
       // Reversing to get the start node at the start
       result.path = result.path.reverse();
-      console.log("Found path.")
+      console.log("Found path. ---------------------------------------------------")
       return result;
     }
     // If we're not at a goal node, set current node to closed, and check the neighbours
     closedSet.push(current.node);
     var neighbours = graph.outgoingEdges(current.node);
     for (let n of neighbours) {
+
       // if they're checked, continue to next
-      if (closedSet.indexOf(n.from) > -1) {
+      if (closedSet.indexOf(n.to) > -1) {
         continue
       }
       // Temporary g-score for the the neighbouring node. current g-score plus the
@@ -135,7 +135,12 @@ function aStarSearch<Node>(
       var t_gScore: number = current.g + n.cost;
       // create a new node, where current is the parent, f and g are given by  the temporary g-score
       var next = new NodeScore(current, n.to, heuristics(n.to) + t_gScore, t_gScore);
+
+
+      // node compare questionable
       var existing = openSet.elementAtIndex(openSet.indexOf(next,cmp));
+
+
       // if the node isn't in the open set, add it.
       // if it is, check if the score is better.
       // If score is better, update the node.
@@ -152,99 +157,4 @@ function aStarSearch<Node>(
   // if we get here, we didn't get to a goal node.
   console.log("COULDN*T FIND PATH")
   return undefined;
-}
-
-
-
-
-//////////////////////////////////////////////////////////////////////
-// here is an example graph
-
-interface Coordinate {
-  x: number;
-  y: number;
-}
-
-
-class GridNode {
-  constructor(
-    public pos: Coordinate
-    ) { }
-
-  add(delta: Coordinate): GridNode {
-    return new GridNode({
-      x: this.pos.x + delta.x,
-      y: this.pos.y + delta.y
-    });
-  }
-
-  compareTo(other: GridNode): number {
-    return (this.pos.x - other.pos.x) || (this.pos.y - other.pos.y);
-  }
-
-  toString(): string {
-    return "(" + this.pos.x + "," + this.pos.y + ")";
-  }
-}
-
-/** Example Graph. */
-class GridGraph implements Graph<GridNode> {
-  private walls: collections.Set<GridNode>;
-
-  constructor(
-    public size: Coordinate,
-    obstacles: Coordinate[]
-    ) {
-    this.walls = new collections.Set<GridNode>();
-    for (var pos of obstacles) {
-      this.walls.add(new GridNode(pos));
-    }
-    for (var x = -1; x <= size.x; x++) {
-      this.walls.add(new GridNode({ x: x, y: -1 }));
-      this.walls.add(new GridNode({ x: x, y: size.y }));
-    }
-    for (var y = -1; y <= size.y; y++) {
-      this.walls.add(new GridNode({ x: -1, y: y }));
-      this.walls.add(new GridNode({ x: size.x, y: y }));
-    }
-  }
-
-  outgoingEdges(node: GridNode): Edge<GridNode>[] {
-    var outgoing: Edge<GridNode>[] = [];
-    for (var dx = -1; dx <= 1; dx++) {
-      for (var dy = -1; dy <= 1; dy++) {
-        if (!(dx == 0 && dy == 0)) {
-          var next = node.add({ x: dx, y: dy });
-          if (!this.walls.contains(next)) {
-            outgoing.push({
-              from: node,
-              to: next,
-              cost: Math.sqrt(dx * dx + dy * dy)
-            });
-          }
-        }
-      }
-    }
-    return outgoing;
-  }
-
-  compareNodes(a: GridNode, b: GridNode): number {
-    return a.compareTo(b);
-  }
-
-  toString(): string {
-    var borderRow = "+" + new Array(this.size.x + 1).join("--+");
-    var betweenRow = "+" + new Array(this.size.x + 1).join("  +");
-    var str = "\n" + borderRow + "\n";
-    for (var y = this.size.y - 1; y >= 0; y--) {
-      str += "|";
-      for (var x = 0; x < this.size.x; x++) {
-        str += this.walls.contains(new GridNode({ x: x, y: y })) ? "## " : "   ";
-      }
-      str += "|\n";
-      if (y > 0) str += betweenRow + "\n";
-    }
-    str += borderRow + "\n";
-    return str;
-  }
 }
